59cfafb8f8931a48c27ecc2649269582bea95c9f
paulgkramer <paul.kramer@gmail.com>
2014-09-18 22:44:05 +0100

    new changes

---
+++ fizzbuzz.pl
@@ -0,0 +1,15 @@
+#!/usr/local/bin/perl
+ 
+# for loop execution
+for( $a = 0; $a < 101; $a = $a + 1 ){
+$result = ""
+$answer = (a % 3 == 0)? 
+"Fizz";
+$answer = (a % 5 == 0)?
+"Buzz";
+
+$printme = ($result == "")?
+"$a" : "$answer";
+
+}
+
